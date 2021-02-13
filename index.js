c37eaded2d9c40b9925505d265d36d30a8d76e50
selvamani05 <selvamani05@ymail.com>
2021-02-13 17:57:27 +0530

    progress-bar-in-html-js

---
+++ index.js
@@ -0,0 +1,13 @@
+const container = document.getElementById('js__container');
+const highlight = document.getElementById('js__highlight');
+var containerHeight;
+
+window.onscroll = function(){
+    containerHeight = container.offsetHeight - window.innerHeight;
+    containerPos = container.getBoundingClientRect();
+    diff = containerHeight + containerPos.top;
+    progressPercentage = diff / containerHeight * 100;
+    cssWidth = Math.floor(100 - progressPercentage);
+    highlight.style.width = cssWidth + "%";
+
+}
